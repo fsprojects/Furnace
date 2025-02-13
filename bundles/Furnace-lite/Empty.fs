292536757d3d3607b380c5b2d207181d62721fe7
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-02-13 17:24:35 -0500

    pushing unversioned

---
+++ bundles/Furnace-lite/Empty.fs
@@ -0,0 +1,4 @@
+namespace Furnace
+
+// This project is to bundle Furnace.Core and some default backends into a single project
+// See Furnace.Core for main Furnace code
---
+++ src/Furnace.Backends.Torch/Furnace.Torch.fs
@@ -0,0 +1,33 @@
+﻿namespace Furnace
+
+open Furnace
+open Furnace.Backends.Torch
+open TorchSharp
+
+[<AutoOpen>]
+module TorchExtensions =
+
+    type dsharp with
+
+        /// <summary>
+        /// Creates a new Furnace tensor from the torch tensor.
+        /// </summary>
+        static member fromTorch(tt: torch.Tensor) =
+            Tensor.ofRawTensor(TorchRawTensor(tt))
+
+    type Tensor with
+        /// <summary>
+        /// Converts the primal of a tensor to a torch tensor.
+        /// </summary>
+        /// <remarks>
+        /// If the tensor does not use the Torch backend an exception is raised.
+        ///
+        /// Note that this operation takes the primal of the tensor. This means
+        /// code that converts to Torch tensors will not be differentiable using
+        /// Furnace differentiation capabilities.
+        /// </remarks>
+        member t.toTorch() =
+            match t.primalRaw with
+            | :? TorchRawTensor as trt -> trt.TorchTensor
+            | _ -> failwith $"toTorch: the input is not a Furnace.Backends.Torch tensor, its backend is {t.backend}"
+
